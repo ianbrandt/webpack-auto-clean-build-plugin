ca3eefdaaaf64995211c87a3dcf08234ce2b530b
Ian Brandt <ian@ianbrandt.com>
2017-03-02 11:14:17 -0800

    Started to work on test cases.

--- index.js
+++ index.js
@@ -9,47 +9,29 @@
 
 var fs = require("fs");
 var path = require("path");
-function WebpackAutoCleanBuildPlugin (path) {}
 
-WebpackAutoCleanBuildPlugin.prototype = {
-	previousFiles: {},
-	varructor: WebpackAutoCleanBuildPlugin,
+function WebpackAutoCleanBuildPlugin() {
 
-	apply: function (compiler) {
-		var self = this;
-		compiler.plugin('after-emit', function (compilation, callback) {
-			var options = compiler.options;
-			var stats = compilation.getStats().toJson({
-				hash: true,
-				publicPath: true,
-				assets: true,
-				chunks: false,
-				modules: false,
-				source: false,
-				errorDetails: false,
-				timings: false
-			});
+	this.previousFiles = {};
+}
 
-			var assetsByChunkName = stats.assetsByChunkName;
-			var keys = Object.keys(assetsByChunkName);
-			for (var key in keys) {
-				if (!keys.hasOwnProperty(key)) continue;
-				key = keys[key];
-				var fileName = assetsByChunkName[key];
-				if (Array.isArray(fileName)) {
-					fileName = assetsByChunkName[key][0];
-				}
-				var file = path.resolve(options.output.path, fileName);
-				var prevFile = self.previousFiles[key];
-				if (prevFile && prevFile != file) {
-					fs.unlink(prevFile);
-				}
-				self.previousFiles[key] = file;
-			}
+WebpackAutoCleanBuildPlugin.prototype.apply = function (compiler) {
 
-			callback();
-		});
-	}
+	compiler.plugin('emit', function(compilation, callback) {
+
+		var changedChunks = compilation.chunks.filter(function(chunk) {
+
+			var oldVersion = this.chunkVersions[chunk.name];
+
+			this.chunkVersions[chunk.name] = chunk.hash;
+
+			return chunk.hash !== oldVersion;
+
+		}.bind(this));
+
+		callback();
+
+	}.bind(this));
 };
 
 module.exports = WebpackAutoCleanBuildPlugin;
