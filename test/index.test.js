e1e950796a5e8e9b4f01cd14b13bdca11a084de9
Ian Brandt <ian@ianbrandt.com>
2017-03-03 14:41:42 -0800

    Continued work on test cases.

--- test/index.test.js
+++ test/index.test.js
@@ -9,46 +9,55 @@ var fse = require('fs-extra')
 var path = require('path')
 var webpack = require('webpack')
 
-var AutoCleanBuildPlugin = require('../index.js')
-
-var OUTPUT_DIR = path.join(__dirname, '../tmp')
-
 chai.use(dirtyChai)
+
+var AutoCleanBuildPlugin = require('../index.js')
+var OUTPUT_DIR = path.join(__dirname, '../tmp')
 
 describe('AutoCleanBuildPlugin', function () {
   beforeEach(function (done) {
     fse.removeSync(OUTPUT_DIR)
+    fse.mkdirsSync(OUTPUT_DIR)
     done()
   })
 
-  it('invokes without error', function (done) {
-    var webpackConfig = {
-      entry: path.join(__dirname, 'fixtures/main.js'),
-      output: {
-        path: OUTPUT_DIR,
-        filename: '[name]-[chunkhash].js'
-      },
-      plugins: [
-        new AutoCleanBuildPlugin()
-      ]
-    }
-
-    fse.mkdirsSync(OUTPUT_DIR)
-
+  function assertWithWebpack (done, webpackConfig, assertions) {
     webpack(webpackConfig, function (err, stats) {
-      expect(err).to.be.null()
-
-      expect(stats.hasErrors()).to.be.false()
-
-      fse.readdir(OUTPUT_DIR, function (err, files) {
+      try {
         expect(err).to.be.null()
-
-        expect(files).to.have.lengthOf(1)
-
-        expect(files[0]).to.match(/^main-.*\.js/)
-      })
-
-      done()
+        expect(stats.hasErrors()).to.be.false()
+        assertions()
+        done()
+      } catch (e) {
+        done(e)
+      }
     })
+  }
+
+  var nominalWebpackConfig = {
+    entry: path.join(__dirname, 'fixtures/main.js'),
+    output: {
+      path: OUTPUT_DIR,
+      filename: '[name]-[chunkhash].js'
+    },
+    plugins: [
+      new AutoCleanBuildPlugin()
+    ]
+  }
+
+  function nominalAssertions () {
+    var files = fse.readdirSync(OUTPUT_DIR)
+    expect(files).to.have.lengthOf(1)
+    expect(files[0]).to.match(/^main-.*\.js/)
+  }
+
+  it('invokes without error', function (done) {
+    assertWithWebpack(done, nominalWebpackConfig, nominalAssertions)
+  })
+
+  it('should remove old hashed files', function (done) {
+    // TODO: Implement!
+    // fse.copySync('test/fixtures/main-oldhash.js', path.join(OUTPUT_DIR, 'main-oldhash.js'))
+    assertWithWebpack(done, nominalWebpackConfig, nominalAssertions)
   })
 })
